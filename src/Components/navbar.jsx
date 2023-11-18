934949435c8a6f7310390bf2db8a77be9dcfd7e8
Nathan Hu <nathanhu@Mack.local>
2023-11-18 02:19:03 -0500

    updated navbar so it can change display depending on if ur logged in or not

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -3,19 +3,47 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 
 function NavBar() {
-  return (
+    let page = "dashboard";
+    let profile = "Zachary Lai";
+    let loggedIn = true;
+    let pageType = pageID(profile)
+    
+    console.log(pageType)
+    return (
     <Navbar className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">Roommate Panic v1.0</Navbar.Brand>
         <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Zachary Lai</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
+          <BarRight pageType = {pageType} profile = {profile} loggedIn = {loggedIn}/>
       </Container>
     </Navbar>
   );
+}
+
+const pageID = (pageType) => {
+    if ((pageType == "dashboard") || ("homepage") || ("taskpage")){
+        return 1;
+    }
+}
+
+const BarRight = (pageData) => {
+    console.log(pageData.pageType);
+    if (pageData.pageType == 1){
+        return(<Navbar.Text>
+           <Login profile = {pageData.profile} loggedIn = {pageData.loggedIn} />
+          </Navbar.Text>);
+    }
+}
+
+const Login = (profile) => {
+    let name = profile.profile;
+    let loggedIn = profile.loggedIn;
+    if (loggedIn == false){
+        return(<a href="#login">Login</a>);
+    }
+    else{
+        return(<Navbar.Collapse className="justify-content-end">Signed in as: <a href="#profile">{name}</a><a href="#logout">Logout</a></Navbar.Collapse>);
+    }
 }
 
 export default NavBar;
