c4d6348bddbb963d4384b59003295e6792966b72
Nathan Hu <nathanhu@Mack.local>
2023-11-18 14:17:41 -0500

    added elements for dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,8 +5,71 @@ const Dashboard = () => {
     });
     return (
         <div>
-            <h1>Dashboard</h1>
-            <p>Dashboard!</p>
+            <div class="row">
+            <div class="card w-50">
+                <div class="card-body">
+                <h1>Your Tasks</h1>
+                <div class="progress">
+                    <div class="progress-bar" role="progressbar" style={{width: '69%'}} aria-valuenow="69" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                    <div class="form-check">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                Default checkbox
+                            </label>
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        </input>
+                   </div>
+                    <div class="form-check">
+                        <label class="form-check-label" for="flexCheckChecked">
+                            Checked checkbox
+                        </label>
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                        </input>
+                    </div>
+                    <a href="#" class="btn btn-primary">View Tasks</a>
+                </div>
+            </div>
+            <div class="card w-50">
+                <div class="card-body">
+                <h1>Your Stats</h1>
+                    <div class="form-check">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                Default checkbox
+                            </label>
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        </input>
+                   </div>
+                    <div class="form-check">
+                    <   label class="form-check-label" for="flexCheckChecked">
+                            Checked checkbox
+                        </label>
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                        </input>
+                    </div>
+                    <a href="#" class="btn btn-primary">View Full Stats</a>
+                </div>
+            </div>
+            </div>
+            <div class="card w-50">
+                <div class="card-body">
+                <h1>Your Roommates</h1>
+                    <div class="form-check">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                Default checkbox
+                            </label>
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        </input>
+                   </div>
+                    <div class="form-check">
+                    <   label class="form-check-label" for="flexCheckChecked">
+                            Checked checkbox
+                        </label>
+                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+                        </input>
+                    </div>
+                    <a href="#" class="btn btn-primary">Manage Roommates</a>
+                </div>
+            </div>
         </div>
     );
 };
