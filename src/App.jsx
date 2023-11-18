4338b408aad9fbcf2240984b70d72b695417a7b3
Alb33rt <albert931023@gmail.com>
2023-11-18 13:33:38 -0500

    Resolve router issue

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
     if (user) {
       setUserProfile({
         loggedIn: true,
-        username: ''
+        username: user
       });
     } else {
       setUserProfile({
@@ -47,7 +47,7 @@ const App = () => {
     <BrowserRouter>
       <NavBar handleLogin={signInWithGoogle} handleLogout={signOutWithGoogle} userProfile={userProfile} />
       <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/tasks" element={<Tasks />} />
       </Routes>
