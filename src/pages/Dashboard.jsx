4ccb9bc6768d1a673cb8e8be66b61a24ff34b15f
Nathan Hu <nathanhu@Mack.local>
2023-11-18 23:11:23 -0500

    Added all the website pages

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -27,28 +27,4 @@ const Dashboard = () => {
     );
 };
 
-const Roommates = () => {
-    return(<div class="card w-100">
-    <div class="card-body">
-    <h1>Your Roommates</h1>
-        <div class="form-check">
-                <label class="form-check-label" for="flexCheckDefault">
-                    Default checkbox
-                </label>
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-            </input>
-       </div>
-        <div class="form-check">
-        <   label class="form-check-label" for="flexCheckChecked">
-                Checked checkbox
-            </label>
-            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" defaultChecked>
-            </input>
-        </div>
-        <a href="#" class="btn btn-primary">Manage Roommates</a>
-    </div>
-    </div>
-    );
-}
-
 export default Dashboard;
---
+++ src/pages/Roommates.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Roommates = () => {
+    return (
+        <div>
+            <h1>Roommates</h1>
+            <p>Roommates!</p>
+        </div>
+    );
+};
+export default Roommates;
