29f8dca21801b189ec7d00c5ab209e079e1b305e
Nathan Hu <nathanhu@Mack.local>
2023-11-19 11:56:56 -0500

    started roommates page

--- src/pages/ManageRoommates.jsx
+++ src/pages/ManageRoommates.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
-const Roommates = () => {
+import React, { useEffect } from 'react';
+import { Container} from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import { Stack } from "react-bootstrap";
+
+import StatSummaryCard from '../components/StatSummaryCard.jsx';
+import StatTopCompletion from '../components/StatTopCompletion.jsx';
+import StatRoommateComp from '../components/StatRoommateComp.jsx';
+
+const Stats = () => {
+    useEffect(() => {
+        document.title = 'My Stats';
+    });
     return (
         <div>
-            <h1>Roommates</h1>
-            <p>Roommates!</p>
+            <Container className="mt-3 justify-content-center">
+                <Row>
+                    <Col>
+                    <StatSummaryCard />
+                    </Col>
+                    <Col>
+                        <Stack gap={3}>
+                        <StatTopCompletion />
+                        <StatRoommateComp />
+                        </Stack>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
-export default Roommates;
+
+export default Stats;
