ad375e4870e2d84e1ff6bc0b747a6330b6ac07c4
Nathan Hu <nathanhu@Mack.local>
2023-11-19 00:13:25 -0500

    bold button

--- src/components/LogoutNavElement.jsx
+++ src/components/LogoutNavElement.jsx
@@ -3,7 +3,7 @@ import { Button, Navbar } from 'react-bootstrap';
 const LogoutNavElement = (props) => {
     return (
         <Navbar.Text>
-            <Button onClick={props.handleLogout} variant="dark"><b>Logout as { props.displayName }</b></Button>
+            <Button style={{color: "white"}} href="/" onClick={props.handleLogout} variant="dark"><b>Log out as {props.displayName}</b></Button>
         </Navbar.Text>
     )
 }
